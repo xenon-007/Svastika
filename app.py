c0aff6dc530cefe449ce33914c7d6fb69b655be6
Priyanshu Kumar Behera <118827995+xenon-007@users.noreply.github.com>
2024-12-22 18:48:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -104,4 +104,5 @@ def analyze_frame():
     return jsonify({"posture_status": "Bad Posture (No Face Detected)"})
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 10000))  # Default to 5000 if not set
+    app.run(host="0.0.0.0", port=port)
